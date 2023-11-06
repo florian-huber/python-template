030a6405b33909bd4a56bda17dc99360f1e3fc8c
Florian Huber <florian.huber@hs-duesseldorf.de>
2023-11-06 12:42:16 +0100

    fix wrong setup structure

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import os
+from setuptools import setup
 
-setup(
-    name="{{ cookiecutter.package_name }}",
-    version="{{ cookiecutter.version }}",
-    packages=find_packages(),
-    install_requires=[
-        # Define your project's dependencies here, e.g.,
-        # 'requests',
-    ],
-    extras_require={
-        "dev": [
-            "black",
-            "isort",
-            "pylint",  # or "ruff" if you choose to use ruff
-            "pytest",  # if you're using pytest for tests
-        ]
-    },
-)
+
+# see setup.cfg
+setup()
